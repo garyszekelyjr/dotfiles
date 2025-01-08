f319c2449513a5ecea0f0ef134cd07b7238ad224
Gary Szekely <gary.szekely.jr@gmail.com>
2025-01-08 10:28:55 -0500

    changes

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,13 +1,15 @@
 return {
 	'saghen/blink.cmp',
 	dependencies = 'rafamadriz/friendly-snippets',
-	version = 'v0.*',
+	version = '*',
 	keymap = { preset = 'default' },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = 'mono'
 		},
-		signature = { enabled = true }
-	}
+		signature = { enabled = true },
+	},
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -29,7 +29,7 @@ return {
 
 		vim.keymap.set("n", "<Space>fb", require("telescope.builtin").buffers)
 		vim.keymap.set("n", "<Space>fh", require("telescope.builtin").help_tags)
-		vim.keymap.set("n", "<Space>ff", require("telescope.builtin").find_files)
+		vim.keymap.set("n", "<Space><Space>", require("telescope.builtin").find_files)
 		vim.keymap.set("n", "<Space>fg", require("telescope.builtin").live_grep)
 		vim.keymap.set("n", "<Space>fc", function()
 			require("telescope.builtin").find_files { cwd = vim.fn.stdpath("config") }
