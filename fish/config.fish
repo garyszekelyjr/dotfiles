4a182e02a830314b8b208f9412f65de2780cdca3
Gary Szekely <gary.szekely.jr@gmail.com>
2025-02-04 18:31:48 -0500

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,17 @@ end
 
 # Added by `rbenv init` on Tue Jan 28 16:32:45 EST 2025
 status --is-interactive; and rbenv init - --no-rehash fish | source
+
+alias activate "source .venv/bin/activate.fish"
+
+function sgit
+	switch $argv[1]
+		case fc
+			git branch | grep $argv[2] | xargs git checkout
+		case sc
+			git add .
+			git commit -m $argv[2]
+		case '*'
+			git $argv
+	end
+end
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,54 +1,48 @@
 return {
-	{
-		"github/copilot.vim",
-		enabled = true
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			'saghen/blink.cmp',
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						{
-							path = "${3rd}/luv/library",
-							words = { "vim%.uv" }
-						}
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		'saghen/blink.cmp',
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{
+						path = "${3rd}/luv/library",
+						words = { "vim%.uv" }
 					}
 				}
 			}
-		},
-		opts = {
-			lsps = {
-				bashls = {},
-				cssls = {},
-				docker_compose_language_service = {},
-				dockerls = {},
-				gopls = {},
-				html = {},
-				htmx = {},
-				jsonls = {},
-				lemminx = {},
-				lua_ls = {},
-				marksman = {},
-				pyright = {},
-				ruby_lsp = {},
-				ruff = {},
-				rust_analyzer = {},
-				svelte = {},
-				tailwindcss = {},
-				taplo = {},
-				ts_ls = {},
-				zls = {}
-			}
-		},
-		config = function(_, opts)
-			for lsp, config in pairs(opts.lsps) do
-				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				require("lspconfig")[lsp].setup(config)
-			end
+		}
+	},
+	opts = {
+		lsps = {
+			bashls = {},
+			cssls = {},
+			docker_compose_language_service = {},
+			dockerls = {},
+			gopls = {},
+			html = {},
+			htmx = {},
+			jsonls = {},
+			lemminx = {},
+			lua_ls = {},
+			marksman = {},
+			pyright = {},
+			ruby_lsp = {},
+			ruff = {},
+			rust_analyzer = {},
+			svelte = {},
+			tailwindcss = {},
+			taplo = {},
+			ts_ls = {},
+			zls = {}
+		}
+	},
+	config = function(_, opts)
+		for lsp, config in pairs(opts.lsps) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			require("lspconfig")[lsp].setup(config)
 		end
-	}
+	end
 }
