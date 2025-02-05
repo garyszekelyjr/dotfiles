31d6747c5368308cb9173098326a688d20878a00
Gary Szekely <gary.szekely.jr@gmail.com>
2025-02-05 14:44:55 -0500

    changes

--- sync.fish
+++ sync.fish
@@ -1,21 +1,9 @@
 set DIR ( realpath (status dirname) ) 
 
-if test -e $HOME/.tmux.conf
-	rm -f $HOME/.tmux.conf
-end
-
-if test -e $HOME/.wezterm.lua
-	rm -f $HOME/.wezterm.lua
-end
-
-if test -e $HOME/.config/fish
-	rm -rf $HOME/.config/fish
-end
-
-
-if test -e $HOME/.config/nvim
-	rm -rf $HOME/.config/nvim
-end
+rm -f $HOME/.tmux.conf
+rm -f $HOME/.wezterm.lua
+rm -rf $HOME/.config/fish
+rm -rf $HOME/.config/nvim
 
 ln -s $DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
 ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
