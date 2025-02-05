08f0205cc747d08a65bbf1e9c1fd3d6dae5c85c8
Gary Szekely <gszekely@edmundsgovtech.com>
2025-02-05 10:40:13 -0500

    use script root

--- sync.ps1
+++ sync.ps1
@@ -2,6 +2,6 @@ rm $HOME\.wezterm.lua -Force
 
 rm -r $env:LOCALAPPDATA\nvim -Force
 
-New-Item -ItemType SymbolicLink -Path $HOME\.wezterm.lua -Target $HOME\.dotfiles\wezterm\.wezterm.lua
+New-Item -ItemType SymbolicLink -Path $HOME\.wezterm.lua -Target $PSScriptRoot\wezterm\.wezterm.lua
 
-New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim -Target $HOME\.dotfiles\nvim
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim -Target $PSScriptRoot\nvim
