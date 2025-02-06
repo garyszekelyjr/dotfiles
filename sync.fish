f71dd223a0dd43499896a461e5004137b1d385a9
Gary Szekely <gary.szekely.jr@gmail.com>
2025-02-06 16:45:46 -0500

    change

--- sync.fish
+++ sync.fish
@@ -4,8 +4,10 @@ rm -f $HOME/.tmux.conf
 rm -f $HOME/.wezterm.lua
 rm -rf $HOME/.config/fish
 rm -rf $HOME/.config/nvim
+rm -rf "$HOME/Library/Application Support/nushell"
 
 ln -s $DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
 ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DIR/fish $HOME/.config/fish
+ln -s $DIR/nushell "$HOME/Library/Application Support/nushell"
 ln -s $DIR/nvim $HOME/.config/nvim
