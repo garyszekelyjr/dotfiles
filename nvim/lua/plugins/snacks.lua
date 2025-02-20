1d969821f911a1289936ae1e8ac210882966230b
Gary Szekely <gszekely@edmundsgovtech.com>
2025-02-20 13:56:11 -0500

    changes

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,13 +1,21 @@
 return {
 	"folke/snacks.nvim",
 	keys = {
-		{ "<Leader>ff", ":lua Snacks.explorer.open()<CR>" },
-		{ "`",          ":lua Snacks.terminal.toggle()<CR>" }
+		{ "<Leader>x",       function() Snacks.bufdelete.all() end },
+		{ "<Leader><Space>", function() Snacks.picker.smart() end },
+		{ "<Leader>b",       function() Snacks.picker.buffers() end },
+		{ "<Leader>c",       function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end },
+		{ "<Leader>e",       function() Snacks.picker.explorer() end },
+		{ "<Leader>g",       function() Snacks.picker.grep() end },
+		{ "<Leader>h",       function() Snacks.picker.help() end },
+		{ "<Leader>gd",      function() Snacks.picker.git_diff() end },
+		{ "<Leader>`",       function() Snacks.terminal.toggle() end }
 	},
 	---@type snacks.Config
 	opts = {
-		explorer = {},
+		bufdelete = {},
 		indent = {},
+		picker = {},
 		terminal = {}
 	}
 }
