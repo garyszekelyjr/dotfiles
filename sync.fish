b15305e26797c6d3a7f9e943253eaca234eb7614
Gary Szekely <gszekely@edmundsgovtech.com>
2025-03-17 16:11:18 -0400

    changes

--- sync.fish
+++ sync.fish
@@ -1,14 +1,11 @@
 set DIR ( realpath (status dirname) )
 
-rm -f $HOME/.wezterm.lua
-
+rm -rf $HOME/.wezterm.lua
 rm -rf $HOME/.lintr
 rm -rf $HOME/.config/fish
 rm -rf $HOME/.config/nvim
-rm -rf "$HOME/Library/Application Support/nushell"
 
 ln -s $DIR/lintr/.lintr $HOME/.lintr
 ln -s $DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
 ln -s $DIR/fish $HOME/.config/fish
-ln -s $DIR/nushell "$HOME/Library/Application Support/nushell"
 ln -s $DIR/nvim $HOME/.config/nvim
--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,2 @@
 Remove-Item "$env:LOCALAPPDATA\nvim" -Recurse -Force
-Remove-Item "$env:APPDATA\nushell" -Recurse -Force
-
 New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\nvim"
-New-Item -ItemType SymbolicLink -Path "$env:APPDATA\nushell" -Target "$PSScriptRoot\nushell"
