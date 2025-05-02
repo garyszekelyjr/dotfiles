67cf8fd8fdd930b69faa814cf4b698013b7afbc0
Gary Szekely <gary.szekely.jr@gmail.com>
2025-05-02 16:10:33 -0400

    added git config

--- sync.fish
+++ sync.fish
@@ -1,13 +1,15 @@
 set DIR ( realpath (status dirname) )
 
-rm -rf $HOME/.wezterm.lua
-rm -rf $HOME/.lintr
-rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.config/fish
 rm -rf $HOME/.config/nvim
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.lintr
+rm -rf $HOME/.tmux.conf
+rm -rf $HOME/.wezterm.lua
 
 ln -s $DIR/fish $HOME/.config/fish
-ln -s $DIR/lintr/.lintr $HOME/.lintr
 ln -s $DIR/nvim $HOME/.config/nvim
+ln -s $DIR/git/.gitconfig $HOME/.gitconfig
+ln -s $DIR/lintr/.lintr $HOME/.lintr
 ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
