959e9f6ed217c5a818742072978678ce90f97297
Gary Szekely <gary.szekely.jr@gmail.com>
2025-05-02 16:24:29 -0400

    changes

--- sync.fish
+++ sync.fish
@@ -13,3 +13,5 @@ ln -s $DIR/git/.gitconfig $HOME/.gitconfig
 ln -s $DIR/lintr/.lintr $HOME/.lintr
 ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
+
+cp $DIR/nixos/configuration.nix /etc/nixos/configuration.nix
