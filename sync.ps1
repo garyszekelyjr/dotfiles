cb275db5a9c995fb2305760a0dc702e4c06dcf39
Gary Szekely <gary.szekely.jr@gmail.com>
2025-06-20 13:14:28 -0400

    sync gitconfig for windows devices

--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,7 @@
 Remove-Item "$HOME\Documents\PowerShell" -Recurse -Force
 Remove-Item "$env:LOCALAPPDATA\nvim" -Recurse -Force
+Remove-Item "$HOME\.gitconfig" -Force
 
 New-Item -ItemType SymbolicLink -Path "$HOME\Documents\PowerShell" -Target "$PSScriptRoot\powershell"
 New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\nvim"
+New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\git\.gitconfig"
