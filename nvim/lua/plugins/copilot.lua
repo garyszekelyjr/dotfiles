b81f01db6fadb41b316377ccf316e5964490261c
Gary Szekely <gary.szekely.jr@gmail.com>
2025-07-16 16:51:40 -0400

    formatted

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
 	"github/copilot.vim",
 	enabled = function()
 		-- ON WINDOWS
-		if package.config:sub(1,1) == "\\" then
+		if package.config:sub(1, 1) == "\\" then
 			return true
 		else
 			return false
