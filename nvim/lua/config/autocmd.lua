055a3bfadcb488c8aaa5fac78052a90635892c98
Gary Szekely <gary.szekely.jr@gmail.com>
2025-07-23 14:52:31 -0400

    changes

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,9 +1,6 @@
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function()
-		vim.cmd("topleft OutlineOpen!")
-		vim.cmd("OutlineRefresh")
-	end
-})
+vim.api.nvim_create_autocmd("VimEnter", { command = "topleft OutlineOpen!" })
+
+vim.api.nvim_create_autocmd("LspAttach", { command = "OutlineRefresh" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function(args)
