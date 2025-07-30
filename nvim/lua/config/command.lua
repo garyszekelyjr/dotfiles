90813e794849d12a839de94d415fb825fde80e6a
Gary Szekely <gary.szekely.jr@gmail.com>
2025-07-30 14:23:06 -0400

    changes

--- nvim/lua/config/command.lua
+++ nvim/lua/config/command.lua
@@ -7,14 +7,16 @@ vim.api.nvim_create_user_command("Comment", function(opts)
 	local comment = comments[vim.bo.filetype] or "//"
 
 	local line = vim.fn.getline(opts.line1):gsub("%s+", "")
-	local num_spaces = vim.fn.indent(opts.line1)
+	local num_spaces = vim.fn.indent(opts.line1) / 8
 
 	if line:sub(1, #comment) == comment then
 		vim.cmd(opts.line1 .. "," .. opts.line2 .. "s:^\\(\\s\\{-\\}\\)" .. comment .. " :\\1:")
 		vim.cmd("noh")
 	else
 		vim.cmd(opts.line1 ..
-			"," .. opts.line2 .. "s:^\\s*\\(.*\\):" .. comment .. " :")
+			"," ..
+			opts.line2 ..
+			"s:^\\(\\s\\{" .. num_spaces .. "\\}\\):\\1" .. comment .. " :")
 		vim.cmd("noh")
 	end
 end, { range = true })
