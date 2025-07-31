7d23cd91bb006f2598126b74aaf79c648d5de94f
Gary Szekely <gary.szekely.jr@gmail.com>
2025-07-31 09:57:09 -0400

    desktop setup and fixes for nvim comments

--- nvim/lua/config/command.lua
+++ nvim/lua/config/command.lua
@@ -7,7 +7,11 @@ vim.api.nvim_create_user_command("Comment", function(opts)
 	local comment = comments[vim.bo.filetype] or "//"
 
 	local line = vim.fn.getline(opts.line1):gsub("%s+", "")
-	local num_spaces = vim.fn.indent(opts.line1) / 8
+	local num_spaces = vim.fn.indent(opts.line1)
+
+	if vim.opt.tabstop:get() == 8 then
+		num_spaces = num_spaces / 8
+	end
 
 	if line:sub(1, #comment) == comment then
 		vim.cmd(opts.line1 .. "," .. opts.line2 .. "s:^\\(\\s\\{-\\}\\)" .. comment .. " :\\1:")
--- sync.fish
+++ sync.fish
@@ -1,14 +1,20 @@
 set DIR ( realpath (status dirname) )
 
 rm -rf $HOME/.config/fish
+rm -rf $HOME/.config/kanshi
 rm -rf $HOME/.config/nvim
+rm -rf $HOME/.config/sway
+rm -rf $HOME/.config/waybar
 rm -rf $HOME/.gitconfig
 rm -rf $HOME/.lintr
 rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.wezterm.lua
 
 ln -s $DIR/fish $HOME/.config/fish
+ln -s $DIR/kanshi $HOME/.config/kanshi
 ln -s $DIR/nvim $HOME/.config/nvim
+ln -s $DIR/sway $HOME/.config/sway
+ln -s $DIR/waybar $HOME/.config/waybar
 ln -s $DIR/git/.gitconfig $HOME/.gitconfig
 ln -s $DIR/lintr/.lintr $HOME/.lintr
 ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
