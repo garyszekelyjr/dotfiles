71d544984a0ce19a61325a3c934a7884992a1355
Gary Szekely <gary.szekely.jr@gmail.com>
2025-10-31 10:23:08 -0400

    changes

--- sync.ps1
+++ sync.ps1
@@ -2,8 +2,12 @@ Remove-Item "$HOME\Documents\PowerShell" -Recurse -Force
 Remove-Item "$env:LOCALAPPDATA\nvim" -Recurse -Force
 Remove-Item "$HOME\.gitconfig" -Force
 Remove-Item "$HOME\.ideavimrc" -Force
+Remove-Item "$HOME\.wezterm.lua" -Force
+Remove-Item "$HOME\.wslconfig" -Force
 
 New-Item -ItemType SymbolicLink -Path "$HOME\Documents\PowerShell" -Target "$PSScriptRoot\powershell"
 New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\nvim"
 New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\git\.gitconfig"
 New-Item -ItemType SymbolicLink -Path "$HOME\.ideavimrc" -Target "$PSScriptRoot\idea\.ideavimrc"
+New-Item -ItemType SymbolicLink -Path "$HOME\.wezterm.lua" -Target "$PSScriptRoot\wezterm\.wezterm.lua"
+New-Item -ItemType SymbolicLink -Path "$HOME\.wslconfig" -Target "$PSScriptRoot\wsl\.wslconfig"
---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,87 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.enable_wayland = false;
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 11
+config.default_cursor_style = "SteadyBar"
+
+-- ON WINDOWS
+if package.config:sub(1, 1) == "\\" then
+	config.font_size = 10
+	config.default_prog = { "pwsh.exe" }
+end
+
+
+config.leader = {
+	key = "Space",
+	mods = "CTRL",
+	timeout_milliseconds = 2000
+}
+
+config.keys = {
+	{
+		key = "t",
+		mods = "LEADER",
+		action = wezterm.action { SpawnTab = "CurrentPaneDomain" }
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action { CloseCurrentPane = { confirm = true } }
+	},
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal {}
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical {}
+	},
+	{
+		key = "LeftArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection "Left",
+	},
+	{
+		key = "RightArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection "Right",
+	},
+	{
+		key = "UpArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection "Up",
+	},
+	{
+		key = "DownArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection "Down",
+	},
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.MoveTabRelative(-1)
+	},
+	{
+		key = "]",
+		mods = "LEADER",
+		action = wezterm.action.MoveTabRelative(1)
+	}
+
+}
+
+for i = 1, 9 do
+	config.keys[#config.keys + 1] = {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action { ActivateTab = i - 1 }
+	}
+end
+
+return config
