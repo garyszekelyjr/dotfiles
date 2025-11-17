f781628e5881b0c3a9175642bd33ebe9cb8abf32
Gary Szekely <gary.szekely.jr@gmail.com>
2025-11-17 10:38:47 -0500

    changes

--- sync.fish
+++ sync.fish
@@ -1,23 +1,13 @@
 set DIR ( realpath (status dirname) )
 
-rm -rf $HOME/.config/fish
-rm -rf $HOME/.config/foot
-rm -rf $HOME/.config/htop
-rm -rf $HOME/.config/kanshi
-rm -rf $HOME/.config/nvim
-rm -rf $HOME/.config/sway
-rm -rf $HOME/.config/waybar
-rm -rf $HOME/.gitconfig
-rm -rf $HOME/.lintr
-rm -rf $HOME/.tmux.conf
-
-ln -s $DIR/fish $HOME/.config/fish
-ln -s $DIR/foot $HOME/.config/foot
-ln -s $DIR/kanshi $HOME/.config/kanshi
-ln -s $DIR/htop $HOME/.config/htop
-ln -s $DIR/nvim $HOME/.config/nvim
-ln -s $DIR/sway $HOME/.config/sway
-ln -s $DIR/waybar $HOME/.config/waybar
-ln -s $DIR/git/.gitconfig $HOME/.gitconfig
-ln -s $DIR/lintr/.lintr $HOME/.lintr
-ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+rm -rf $HOME/.gitconfig     && ln -s $DIR/git/.gitconfig $HOME/.gitconfig
+rm -rf $HOME/.config/fish   && ln -s $DIR/fish $HOME/.config/fish
+rm -rf $HOME/.config/foot   && ln -s $DIR/foot $HOME/.config/foot
+rm -rf $HOME/.config/htop   && ln -s $DIR/htop $HOME/.config/htop
+rm -rf $HOME/.config/hypr   && ln -s $DIR/hypr $HOME/.config/hypr
+rm -rf $HOME/.config/kanshi && ln -s $DIR/kanshi $HOME/.config/kanshi
+rm -rf $HOME/.config/kitty  && ln -s $DIR/kitty $HOME/.config/kitty
+rm -rf $HOME/.lintr         && ln -s $DIR/lintr/.lintr $HOME/.lintr
+rm -rf $HOME/.config/nvim   && ln -s $DIR/nvim $HOME/.config/nvim
+rm -rf $HOME/.tmux.conf     && ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+rm -rf $HOME/.config/waybar && ln -s $DIR/waybar $HOME/.config/waybar
