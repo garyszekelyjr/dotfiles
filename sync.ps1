739432a7344d97be513e23fbf85495c76a9d83d6
Gary Szekely <gary.szekely.jr@gmail.com>
2026-02-19 15:18:28 -0500

    remove neovim config

--- sync.ps1
+++ sync.ps1
@@ -1,4 +1,3 @@
-Remove-Item "$env:LOCALAPPDATA\nvim" -Recurse -Force     && New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\nvim"
 Remove-Item "$HOME\Documents\PowerShell" -Recurse -Force && New-Item -ItemType SymbolicLink -Path "$HOME\Documents\PowerShell" -Target "$PSScriptRoot\powershell"
 Remove-Item "$HOME\.gitconfig" -Force                    && New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\git\.gitconfig"
 Remove-Item "$HOME\.ideavimrc" -Force                    && New-Item -ItemType SymbolicLink -Path "$HOME\.ideavimrc" -Target "$PSScriptRoot\idea\.ideavimrc"
