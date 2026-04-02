d74234490337a249ed8084c28f9473f4ad15e899
Gary Szekely <gary.szekely.jr@gmail.com>
2026-04-02 12:38:13 -0400

    changes

--- nvim/lua/plugins/minuet.lua
+++ nvim/lua/plugins/minuet.lua
@@ -22,14 +22,29 @@ return {
 			provider_options = {
 				openai_fim_compatible = {
 					api_key = function() return "NO_KEY_NEEDED" end,
-					name = "Ollama",
-					-- end_point = "http://10.8.0.1:11434/v1/completions",
-					end_point = "http://localhost:11434/v1/completions",
-					model = "qwen2.5-coder:0.5b",
+					name = "Llama.cpp",
+					--end_point = "http://10.8.0.1:8080/v1/completions",
+					end_point = "http://localhost:8080/v1/completions",
+					-- The model is set by the llama-cpp server and cannot be altered
+					-- post-launch.
+					model = "Qwen2.5-Coder-1.5B",
 					optional = {
 						max_tokens = 56,
 						top_p = 0.9,
 					},
+					-- Llama.cpp does not support the `suffix` option in FIM completion.
+					-- Therefore, we must disable it and manually populate the special
+					-- tokens required for FIM completion.
+					template = {
+						prompt = function(context_before_cursor, context_after_cursor, _)
+							return "<|fim_prefix|>"
+							    .. context_before_cursor
+							    .. "<|fim_suffix|>"
+							    .. context_after_cursor
+							    .. "<|fim_middle|>"
+						end,
+						suffix = false,
+					}
 				},
 				gemini = {
 					model = "gemini-2.0-flash",
